5bd7643d308526c71a7a9893b6b5e0e6c2fd80d8
morninng <mr_morninng_star_dash@yahoo.co.jp>
2017-12-25 14:43:31 +0900

    join room implemented

--- index.js
+++ index.js
@@ -30,15 +30,36 @@ io.sockets.setMaxListeners(Infinity);
 const mixidea_io = io.of('/mixidea')
 
 
-mixidea_io.on('connection',(socket)=>{
+mixidea_io.on('connection',(socket_client)=>{
     console.log("connected to mixidea");
+    socket_client.emit('connected');
 
-    socket.on('disconnect', function(){
-        console.log("disconnected socket id= " + socket.id);
+    socket_client.on('join-room', (room_name) => {
+        console.log('join-room called ', room_name)
+        socket_client.join(room_name);
+    })
+
+    socket_client.on('disconnect', function(){
+        console.log("disconnected socket id= " + socket_client.id);
 
     });
     
 
 });
+
+let count = 0;
+const roomname_arr = ['room_a', 'room_b', 'room_c']
+
+setInterval(()=>{
+    count++;
+
+    const number = count % roomname_arr.length;
+    const room_name = roomname_arr[number];
+    const message = 'message' + String(count);
+    console.log(room_name + ' - ' + message);
+    mixidea_io.to(room_name).emit('message', {room_name, message} );
+
+}, 1000);
+
 
 
