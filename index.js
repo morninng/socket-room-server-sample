7e50fb290c60f1b5d116ad1f50fe9814396176fb
morninng <mr_morninng_star_dash@yahoo.co.jp>
2017-12-25 23:23:24 +0900

    message retrieve from pubnub

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 const express = require('express')
 const app = express();
 const http = require('http');
+const PubNub = require('pubnub');
+
+const pubnub = new PubNub({
+    subscribeKey: "sub-c-1790666a-e0ae-11e7-8b5d-3e564a31a465",
+    publishKey: "pub-c-b7322b72-a22e-4d52-9e1f-b2e40c1c5422",
+    secretKey: "sec-c-NmU4YTA3NDgtZWFlOS00YzljLTllNzktNDVkNjkwZWE1Njdl",
+    ssl: true
+})
+
+pubnub.addListener({   
+    message: function(m) {
+        // console.log(m);
+        // handle message
+        const actualChannel = m.actualChannel;
+        const channelName = m.channel; // The channel for which the message belongs
+        const msg = m.message; // The Payload
+        const publisher = m.publisher;
+        const subscribedChannel = m.subscribedChannel;
+        const channelGroup = m.subscription; // The channel group or wildcard subscription match (if exists)
+        const pubTT = m.timetoken; // Publish timetoken
+
+        publish_message_to_client(channelName, msg)
+    }
+});
+
+pubnub.subscribe({
+    channels: ['aaa', 'bbb', 'ccc', 'ddd']
+});
+
+
+
+function publish_message_to_client(channelName, msg) {
+    console.log('Channel:' +  channelName + ' - msg: ' + msg);
+    mixidea_io.to(channelName).emit('message', {room_name: channelName, message: msg} );
+}
 
 
 
@@ -66,19 +101,19 @@ function check_numberof_clients() {
 }
 
 
-let count = 0;
-const roomname_arr = ['room_a', 'room_b', 'room_c']
+// let count = 0;
+// const roomname_arr = ['room_a', 'room_b', 'room_c']
 
-setInterval(()=>{
-    count++;
+// setInterval(()=>{
+//     count++;
 
-    const number = count % roomname_arr.length;
-    const room_name = roomname_arr[number];
-    const message = 'message' + String(count);
-    console.log(room_name + ' - ' + message);
-    mixidea_io.to(room_name).emit('message', {room_name, message} );
+//     const number = count % roomname_arr.length;
+//     const room_name = roomname_arr[number];
+//     const message = 'message' + String(count);
+//     console.log(room_name + ' - ' + message);
+//     mixidea_io.to(room_name).emit('message', {room_name, message} );
 
-}, 1000);
+// }, 1000);
 
 
 
